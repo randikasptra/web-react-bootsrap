dd32b530e9b40723c00529f9699af483a665d316
randikasptra <saputrarandika857@gmail.com>
2025-03-10 12:36:21 +0700

    Menambah sidebar

---
+++ src/Sidebar.jsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Sidebar(){
+    return(
+        <>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+        </ul>
+        </>
+    )
+}
