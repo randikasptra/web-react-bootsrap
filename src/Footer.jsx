c5574e7f6404fca8214f7dc50792c6217979890c
randikasptra <saputrarandika857@gmail.com>
2025-03-10 12:33:59 +0700

    Menambah list pada footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,6 +5,11 @@ export default function Footer(){
     <>
     <div className="">
         <h2>Ini Footer</h2>
+        <ul>
+            <li>Home</li>
+            <li>Beranda</li>
+            <li>Product</li>
+        </ul>
     </div>
     </>
 }
